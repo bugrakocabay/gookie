930469cba8b0f5fa4ad630ebf52c5e751ba50cd0
Bugra <bugra.kocabay@useinsider.com>
2023-05-17 21:52:15 +0300

    getEncryptionKey

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gookie
 
 go 1.20
+
+require github.com/keybase/go-keychain v0.0.0-20230307172405-3e4884637dd1 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/keybase/go-keychain"
 	"io"
 	"net/http"
 	"time"
@@ -56,6 +57,30 @@ func fetchData(url string) (string, error) {
 	return string(body), nil
 }
 
+func getEncryptionKey() (string, error) {
+	var err error
+
+	query := keychain.NewItem()
+	query.SetSecClass(keychain.SecClassGenericPassword)
+	query.SetService("Chrome Safe Storage")
+	query.SetAccount("Chrome")
+	query.SetMatchLimit(keychain.MatchLimitOne)
+	query.SetReturnData(true)
+	results, err := keychain.QueryItem(query)
+	if err != nil {
+		return "", err
+	} else if len(results) != 1 {
+		return "", fmt.Errorf("password not found")
+	}
+
+	return string(results[0].Data), nil
+}
+
 func main() {
-	getIPData()
+	encryptedKey, err := getEncryptionKey()
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	} else {
+		fmt.Printf("Encrypted Key: %s\n", encryptedKey)
+	}
 }
