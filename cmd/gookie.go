187877e8bc4f5c504e2e83e2b6c02717d0b1300f
bugrakocabay <kocabaybugra@gmail.com>
2023-05-20 13:50:27 +0300

    firefox started

--- cmd/gookie.go
+++ cmd/gookie.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"gookie/pkg/browser"
-	"gookie/pkg/utils"
-)
-
 func main() {
-	cookieData, err := browser.ReadBraveCookies()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = utils.JSONSaver(cookieData, "cookies.json")
-	if err != nil {
-		log.Fatal(err)
-	}
+	
 }
--- pkg/browser/firefox.go
+++ pkg/browser/firefox.go
@@ -1,1 +1,43 @@
 package browser
+
+import (
+	"errors"
+	"gookie/pkg/utils"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func getDefaultFirefoxProfile() (string, error) {
+	osUserData, err := utils.GetOsUserData()
+	if err != nil {
+		return "", err
+	}
+
+	profilesDir := filepath.Join("/Users", osUserData.Username, "Library", "Application Support", "Firefox", "Profiles")
+	profileFiles, err := os.ReadDir(profilesDir)
+	if err != nil {
+		return "", err
+	}
+
+	for _, profile := range profileFiles {
+		if !profile.IsDir() || !strings.Contains(profile.Name(), ".default") {
+			continue
+		}
+
+		currentProfile, err := os.ReadDir(filepath.Join(profilesDir, profile.Name()))
+		if err != nil {
+			return "", err
+		}
+
+		for _, file := range currentProfile {
+			if file.Name() != "cookies.sqlite" {
+				continue
+			}
+
+			return profile.Name(), nil
+		}
+	}
+
+	return "", errors.New("could not find default profile")
+}
