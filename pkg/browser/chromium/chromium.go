0988cdf68be0ced268da8bdc52a2efacda86afb6
bugrakocabay <kocabaybugra@gmail.com>
2023-06-05 21:26:01 +0300

    refactor

--- pkg/browser/chromium/chromium.go
+++ pkg/browser/chromium/chromium.go
@@ -1,16 +1,6 @@
 package chromium
 
 import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"database/sql"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"os"
-	"path/filepath"
 	"syscall"
 	"unsafe"
 
@@ -41,6 +31,14 @@ const (
 		is_secure as Secure, value as Value, encrypted_value as EncryptedValue 
 		FROM cookies;`
 	chromiumPasswordQuery = `SELECT origin_url as URL, username_value as Username, password_value as Password FROM logins;`
+)
+
+var (
+	DLLCrypt32  = syscall.NewLazyDLL(dllCrypt32Name)
+	DLLKernel32 = syscall.NewLazyDLL(dllKernel32Name)
+
+	PCryptUnprotectData = DLLCrypt32.NewProc("CryptUnprotectData")
+	PLocalFree          = DLLKernel32.NewProc("LocalFree")
 )
 
 // Cookie represents a cookie stored by a web browser.
@@ -77,15 +75,6 @@ type JSONStruct struct {
 	EdgeCookies     []Cookie   `json:"edgeCookies"`
 }
 
-var (
-	DLLCrypt32  = syscall.NewLazyDLL(dllCrypt32Name)
-	DLLKernel32 = syscall.NewLazyDLL(dllKernel32Name)
-
-	PCryptUnprotectData = DLLCrypt32.NewProc("CryptUnprotectData")
-	PLocalFree          = DLLKernel32.NewProc("LocalFree")
-	aesKey              []byte
-)
-
 // DataBlob represents a data blob as used by Windows cryptographic functions.
 type DataBlob struct {
 	cbData uint32
@@ -108,112 +97,4 @@ func newBlob(d []byte) *DataBlob {
 		pbData: &d[0],
 		cbData: uint32(len(d)),
 	}
-}
-
-// decryptValue decrypts a value using either AES GCM or Windows CryptUnprotectData.
-func decryptValue(data []byte) ([]byte, error) {
-	if bytes.Equal(data[0:3], []byte{'v', '1', '0'}) {
-		aesBlock, err := aes.NewCipher(aesKey)
-		if err != nil {
-			return nil, fmt.Errorf("error creating cipher block: %w", err)
-		}
-
-		aesGCM, err := cipher.NewGCM(aesBlock)
-		if err != nil {
-			return nil, fmt.Errorf("error creating GCM: %w", err)
-		}
-
-		nonce := data[3:15]
-		encryptedData := data[15:]
-
-		plaintext, err := aesGCM.Open(nil, nonce, encryptedData, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		return plaintext, nil
-
-	} else {
-		var outBlob DataBlob
-		r, _, err := PCryptUnprotectData.Call(uintptr(unsafe.Pointer(newBlob(data))), 0, 0, 0, 0, 0, uintptr(unsafe.Pointer(&outBlob)))
-		if r == 0 {
-			return nil, err
-		}
-		defer PLocalFree.Call(uintptr(unsafe.Pointer(outBlob.pbData)))
-
-		return outBlob.toByteArray(), nil
-	}
-}
-
-// getAesGCMKey retrieves the AES GCM key from a browser's Local State file.
-func getAesGCMKey(keyPath string) error {
-	osUser, err := os.UserHomeDir()
-	if err != nil {
-		return err
-	}
-
-	localStatePath := filepath.Join(osUser, keyPath)
-	data, err := os.ReadFile(localStatePath)
-	if err != nil {
-		return err
-	}
-
-	var localState map[string]interface{}
-	if err = json.Unmarshal(data, &localState); err != nil {
-		return err
-	}
-
-	osCrypt, ok := localState["os_crypt"].(map[string]interface{})
-	if !ok {
-		return errors.New("os_crypt key not found in localState")
-	}
-
-	encryptedKeyStr, ok := osCrypt["encrypted_key"].(string)
-	if !ok {
-		return errors.New("encrypted_key not found in os_crypt")
-	}
-
-	encryptedKey, err := base64.StdEncoding.DecodeString(encryptedKeyStr)
-	if err != nil {
-		return err
-	}
-
-	if !bytes.Equal(encryptedKey[0:5], []byte{'D', 'P', 'A', 'P', 'I'}) {
-		return errors.New("encrypted_key does not look like DPAPI key")
-	}
-
-	encryptedKey, err = decryptValue(encryptedKey[5:])
-	if err != nil {
-		return err
-	}
-
-	aesKey = encryptedKey
-
-	return nil
-}
-
-// queryDatabase is a helper function to make a query from a SQL database.
-func queryDatabase(path, query, keyPath string) (*sql.Rows, error) {
-	osUser, err := os.UserHomeDir()
-	if err != nil {
-		return nil, err
-	}
-	dbPath := filepath.Join(osUser, path)
-
-	dbConn, err := sql.Open("sqlite3", fmt.Sprintf("file:%s?cache=shared&mode=ro", dbPath))
-	if err != nil {
-		return nil, err
-	}
-	defer dbConn.Close()
-
-	err = getAesGCMKey(keyPath)
-	if err != nil {
-		return nil, err
-	}
-	rows, err := dbConn.Query(query)
-	if err != nil {
-		return nil, err
-	}
-
-	return rows, nil
 }
--- pkg/browser/chromium/fetcher.go
+++ pkg/browser/chromium/fetcher.go
@@ -1,11 +1,23 @@
 package chromium
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"database/sql"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"gookie/pkg/utils"
+	"os"
+	"path/filepath"
+	"unsafe"
 )
 
-type BrowserDataFetcher struct{}
+type BrowserDataFetcher struct {
+	aesKey []byte
+}
 
 // NewBrowserDataFetcher creates a new BrowserDataFetcher.
 func NewBrowserDataFetcher() *BrowserDataFetcher {
@@ -70,7 +82,7 @@ func (b *BrowserDataFetcher) GetAllBrowserData() (JSONStruct, error) {
 
 // readChromiumCookies is a method of BrowserDataFetcher that reads and decrypts cookies from a Chromium-based browser.
 func (b *BrowserDataFetcher) readChromiumCookies(path, keyPath string) ([]Cookie, error) {
-	rows, err := queryDatabase(path, chromiumCookieQuery, keyPath)
+	rows, err := b.queryDatabase(path, chromiumCookieQuery, keyPath)
 	if err != nil {
 		return nil, err
 	}
@@ -92,7 +104,7 @@ func (b *BrowserDataFetcher) readChromiumCookies(path, keyPath string) ([]Cookie
 		cookie.IsExpired = utils.IsExpired(expires)
 		cookie.HttpOnly = utils.IntToBool(httpOnly)
 		cookie.IsSecure = utils.IntToBool(secure)
-		decrypted, err := decryptValue(cookie.EncryptedValue)
+		decrypted, err := b.decryptValue(cookie.EncryptedValue)
 		if err != nil {
 			return nil, fmt.Errorf("error decrypting: %w", err)
 		}
@@ -106,7 +118,7 @@ func (b *BrowserDataFetcher) readChromiumCookies(path, keyPath string) ([]Cookie
 
 // readChromiumPasswords is a method of BrowserDataFetcher that reads and decrypts passwords from a Chromium-based browser.
 func (b *BrowserDataFetcher) readChromiumPasswords(path, keyPath string) ([]Password, error) {
-	rows, err := queryDatabase(path, chromiumPasswordQuery, keyPath)
+	rows, err := b.queryDatabase(path, chromiumPasswordQuery, keyPath)
 	if err != nil {
 		return nil, err
 	}
@@ -121,7 +133,7 @@ func (b *BrowserDataFetcher) readChromiumPasswords(path, keyPath string) ([]Pass
 		if err != nil {
 			return nil, fmt.Errorf("error scanning row: %w", err)
 		}
-		decrypted, err := decryptValue(password.PasswordEncrypted)
+		decrypted, err := b.decryptValue(password.PasswordEncrypted)
 		if err != nil {
 			return nil, err
 		}
@@ -130,4 +142,112 @@ func (b *BrowserDataFetcher) readChromiumPasswords(path, keyPath string) ([]Pass
 		passwords = append(passwords, password)
 	}
 	return passwords, nil
+}
+
+// getAesGCMKey retrieves the AES GCM key from a browser's Local State file.
+func (b *BrowserDataFetcher) getAesGCMKey(keyPath string) error {
+	osUser, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	localStatePath := filepath.Join(osUser, keyPath)
+	data, err := os.ReadFile(localStatePath)
+	if err != nil {
+		return err
+	}
+
+	var localState map[string]interface{}
+	if err = json.Unmarshal(data, &localState); err != nil {
+		return err
+	}
+
+	osCrypt, ok := localState["os_crypt"].(map[string]interface{})
+	if !ok {
+		return errors.New("os_crypt key not found in localState")
+	}
+
+	encryptedKeyStr, ok := osCrypt["encrypted_key"].(string)
+	if !ok {
+		return errors.New("encrypted_key not found in os_crypt")
+	}
+
+	encryptedKey, err := base64.StdEncoding.DecodeString(encryptedKeyStr)
+	if err != nil {
+		return err
+	}
+
+	if !bytes.Equal(encryptedKey[0:5], []byte{'D', 'P', 'A', 'P', 'I'}) {
+		return errors.New("encrypted_key does not look like DPAPI key")
+	}
+
+	encryptedKey, err = b.decryptValue(encryptedKey[5:])
+	if err != nil {
+		return err
+	}
+
+	b.aesKey = encryptedKey
+
+	return nil
+}
+
+// decryptValue decrypts a value using either AES GCM or Windows CryptUnprotectData.
+func (b *BrowserDataFetcher) decryptValue(data []byte) ([]byte, error) {
+	if bytes.Equal(data[0:3], []byte{'v', '1', '0'}) {
+		aesBlock, err := aes.NewCipher(b.aesKey)
+		if err != nil {
+			return nil, fmt.Errorf("error creating cipher block: %w", err)
+		}
+
+		aesGCM, err := cipher.NewGCM(aesBlock)
+		if err != nil {
+			return nil, fmt.Errorf("error creating GCM: %w", err)
+		}
+
+		nonce := data[3:15]
+		encryptedData := data[15:]
+
+		plaintext, err := aesGCM.Open(nil, nonce, encryptedData, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		return plaintext, nil
+
+	} else {
+		var outBlob DataBlob
+		r, _, err := PCryptUnprotectData.Call(uintptr(unsafe.Pointer(newBlob(data))), 0, 0, 0, 0, 0, uintptr(unsafe.Pointer(&outBlob)))
+		if r == 0 {
+			return nil, err
+		}
+		defer PLocalFree.Call(uintptr(unsafe.Pointer(outBlob.pbData)))
+
+		return outBlob.toByteArray(), nil
+	}
+}
+
+// queryDatabase is a helper function to make a query from a SQL database.
+func (b *BrowserDataFetcher) queryDatabase(path, query, keyPath string) (*sql.Rows, error) {
+	osUser, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	dbPath := filepath.Join(osUser, path)
+
+	dbConn, err := sql.Open("sqlite3", fmt.Sprintf("file:%s?cache=shared&mode=ro", dbPath))
+	if err != nil {
+		return nil, err
+	}
+	defer dbConn.Close()
+
+	err = b.getAesGCMKey(keyPath)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := dbConn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	return rows, nil
 }
